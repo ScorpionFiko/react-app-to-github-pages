e0eec1b97dfbbc6a8c022bbc2a00c90ad8bd955d
Stefan Marinov <stefan_marinov@rogers.com>
2023-03-20 11:40:42 -0400

    Updates

---
+++ precache-manifest.327c1607857d1bbe27a6611969c5b0cc.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "51dc6f8ec43e7e085670",
+    "url": "/react-app-to-github-pages/static/js/main.6b1c84dd.chunk.js"
+  },
+  {
+    "revision": "e9699b06d9b3ce2079e6",
+    "url": "/react-app-to-github-pages/static/js/runtime~main.9c256e76.js"
+  },
+  {
+    "revision": "4e2aeeb160ebd841bf86",
+    "url": "/react-app-to-github-pages/static/js/2.194daaa2.chunk.js"
+  },
+  {
+    "revision": "f917e91384ae4d7bfbbfdd83da0597d1",
+    "url": "/react-app-to-github-pages/index.html"
+  }
+];
